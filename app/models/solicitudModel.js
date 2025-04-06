32c4d52577d73091ccca0b23c28e85a6e726d210
KarenP <karenpadron0608@gmail.com>
2025-04-05 22:02:59 -0600

    Folio

--- app/models/solicitudModel.js
+++ app/models/solicitudModel.js
@@ -18,4 +18,20 @@ const solicitudSchema = new mongoose.Schema({
     descripcionTrabajo: { type: String, required: true }
 });
 
+// Middleware para generar automáticamente el número de folio
+solicitudSchema.pre('validate', async function(next) {
+    if (this.isNew && !this.numeroFolio) {
+        const empresaPrefix = this.nombreEmpresa.trim().toUpperCase().substring(0, 3);
+
+        // Contar solicitudes existentes con el mismo prefijo
+        const regex = new RegExp(`^${empresaPrefix}-\\d{3}$`);
+        const count = await mongoose.model('Solicitude').countDocuments({ numeroFolio: { $regex: regex } });
+
+        const nextNumber = (count + 1).toString().padStart(3, '0');
+        this.numeroFolio = `${empresaPrefix}-${nextNumber}`;
+    }
+
+    next();
+});
+
 module.exports = mongoose.model('Solicitude', solicitudSchema);
